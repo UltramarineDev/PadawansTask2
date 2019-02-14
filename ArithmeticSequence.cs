e6f9aac2467bc75f5b9a2dd3e5e66cdd474d4b08
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-02-14 12:03:44 +0300

    add initial solution

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -7,7 +7,22 @@ namespace PadawansTask2
         public static int Calculate(int number, int add, int count)
         {
             // put your code here
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            if (count <= 0)
+            {
+                throw new ArgumentException();
+            }
+           int sum = 0;
+           for (int i=0; i< count; i++)
+            {
+                try
+                    { sum += number + i * add;}
+                catch (OverflowException)
+                {
+                    throw new OverflowException();
+                }
+            }
+            return sum;
         }
     }
 }
